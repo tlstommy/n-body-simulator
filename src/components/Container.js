7a4b2b6aa639d9007c895136276bb3cbb91c41a6
lulamae12 <tls.tommy11@gmail.com>
2023-10-04 20:01:25 -0400

    tri point

--- src/components/Container.js
+++ src/components/Container.js
@@ -8,10 +8,9 @@ export default function Container(){
     const bodiesData = [
         { x: 400, y: 300, vX: 0, vY: 0, radius: 20, color: 'yellow', mass: 5000, staticBody: true},
 
-        { x: 500, y: 300, vX: 0, vY: 1.5, radius: 10, color: 'green', mass: 50, staticBody: false},
-        { x: 300, y: 300, vX: 0, vY: -1.5, radius: 10, color: 'red', mass: 50, staticBody: false},
-        //{ x: 300, y: 300, vX: 0, vY: 1.5, radius: 10, color: 'red', mass: 100, staticBody: false},
-
+        { x: 550, y: 450, vX: 0, vY: 0, radius: 10, color: 'green', mass: 50, staticBody: false},
+        { x: 250, y: 450, vX: 0, vY: 0, radius: 10, color: 'red', mass: 50, staticBody: false},
+        { x: 400, y: 90, vX: 0, vY: 0, radius: 10, color: 'blue', mass: 50, staticBody: false},
     ];
 
     
