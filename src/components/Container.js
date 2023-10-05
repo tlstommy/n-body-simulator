a06afd726fc5e7fe3379370a8c585ebaeef56a06
lulamae12 <tls.tommy11@gmail.com>
2023-10-04 20:15:38 -0400

    commit for tonight

--- src/components/Container.js
+++ src/components/Container.js
@@ -8,9 +8,9 @@ export default function Container(){
     const bodiesData = [
         { x: 400, y: 400, vX: 0, vY: 0, radius: 20, color: 'yellow', mass: 5000, staticBody: true},
 
-        { x: 550, y: 550, vX: 0, vY: 0, radius: 10, color: 'green', mass: 50, staticBody: false},
-        { x: 250, y: 550, vX: 0, vY: 0, radius: 10, color: 'red', mass: 50, staticBody: false},
-        { x: 400, y: 190, vX: 0, vY: 0, radius: 10, color: 'blue', mass: 50, staticBody: false},
+        { x: 550, y: 400, vX: 0, vY: 1.5, radius: 10, color: 'green', mass: 50, staticBody: false},
+        { x: 250, y: 400, vX: 0, vY: -1.5, radius: 10, color: 'red', mass: 50, staticBody: false},
+        //{ x: 400, y: 190, vX: 0, vY: 0, radius: 10, color: 'blue', mass: 50, staticBody: false},
     ];
 
     
